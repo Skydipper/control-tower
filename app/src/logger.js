ae8a80c62cc28f90206faaa0c3aff2c24ebf87b3
Tiago Garcia <tiagojsag@gmail.com>
2019-03-15 09:30:18 +0100

    Add test endpoint to generate errors; changes to logging

--- app/src/logger.js
+++ app/src/logger.js
@@ -1,40 +1,25 @@
 const config = require('config');
 const bunyan = require('bunyan');
-const bsyslog = require('bunyan-syslog-udp');
-/**
- * Create Logger
- */
-module.exports = (function createLogger() {
-    const streams = [];
 
-    switch (config.get('logger.type')) {
-        case 'syslog':
-            streams.push({
-                type: 'raw',
-                level: config.get('logger.level') || 'debug',
-                stream: bsyslog.createBunyanStream({
-                    name: config.get('logger.syslog.name'),
-                    host: config.get('logger.syslog.host'),
-                    port: config.get('logger.syslog.port'),
-                    facility: 'local0'
-                })
-            });
-            break;
-        case 'console':
-            streams.push({
-                level: config.get('logger.level') || 'debug',
-                stream: process.stdout,
-            });
-            break;
-        default:
-            break;
+const { LoggingBunyan } = require('@google-cloud/logging-bunyan');
 
-    }
+const streams = [
+    {
+        stream: process.stdout, level: config.get('logger.level') || 'debug'
+    }, {
+        stream: process.stderr, level: 'warn'
+    },
+];
 
-    const logger = bunyan.createLogger({
-        name: config.get('logger.name'),
-        streams,
-    });
-    return logger;
 
-}());
+if (config.get('logger.stackdriver')) {
+    const loggingBunyan = new LoggingBunyan();
+    streams.push(loggingBunyan.stream('info'));
+}
+
+const logger = bunyan.createLogger({
+    name: config.get('logger.name'),
+    streams,
+});
+
+module.exports = logger;
