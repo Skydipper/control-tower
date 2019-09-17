3297494ac907e685d2cce1d664875935cbab0023
Tiago Garcia <tiagojsag@gmail.com>
2019-09-17 14:57:09 +0200

    Improve wording on dispatch tests

--- app/test/e2e/endpoint-dispatch.spec.js
+++ app/test/e2e/endpoint-dispatch.spec.js
@@ -18,19 +18,19 @@ describe('Endpoint dispatch tests', () => {
         microservice = await getTestAgent();
     });
 
-    it('Created endpoint without redirects should return not found', async () => {
+    it('Created endpoint without redirects should return a 404 HTTP code with a "Endpoint not found" message', async () => {
         await createEndpoint({ redirect: [] });
         const result = await microservice.post('/api/v1/dataset');
         ensureCorrectError(result, 'Endpoint not found', 404);
     });
 
-    it('Create endpoint after cache is was declared and version is not updated should return not found', async () => {
+    it('Create endpoint after cache is was declared and version is not updated should return a 404 HTTP code with a "Endpoint not found" message', async () => {
         await createEndpoint();
         const result = await microservice.post('/api/v1/dataset');
         ensureCorrectError(result, 'Endpoint not found', 404);
     });
 
-    it('Created endpoint should executes', async () => {
+    it('Created endpoint should return a 200 HTTP code', async () => {
         await updateVersion();
         await createEndpoint();
         createMockEndpoint('/api/v1/dataset');
@@ -39,14 +39,14 @@ describe('Endpoint dispatch tests', () => {
         result.text.should.equal('ok');
     });
 
-    it('Created endpoint with applicationRequired and not send application should fall', async () => {
+    it('Created endpoint with applicationRequired and not send application should return a 401 HTTP code with a "Required app_key" message', async () => {
         await updateVersion();
         await createEndpoint({ applicationRequired: true });
         const result = await microservice.post('/api/v1/dataset');
         ensureCorrectError(result, 'Required app_key', 401);
     });
 
-    it('Created endpoint with applicationRequired and send app_key should succeed', async () => {
+    it('Created endpoint with applicationRequired and send app_key should return a 200 HTTP code', async () => {
         await updateVersion();
         await createEndpoint({ applicationRequired: true });
         createMockEndpoint('/api/v1/dataset');
@@ -55,7 +55,7 @@ describe('Endpoint dispatch tests', () => {
         result.text.should.equal('ok');
     });
 
-    it('Send queries, queries should move on new POST request', async () => {
+    it('External request\'s query params are passed along to the internal call on POST requests', async () => {
         await updateVersion();
         await createEndpoint();
         createMockEndpoint('/api/v1/dataset?test1=test&test2=test', { method: 'post' });
@@ -64,7 +64,7 @@ describe('Endpoint dispatch tests', () => {
         result.status.should.equal(200);
     });
 
-    it('Send queries, queries should move on new PUT request', async () => {
+    it('External request\'s query params are passed along to the internal call on PUT requests', async () => {
         await updateVersion();
         await createEndpoint(changeMethod('PUT'));
         createMockEndpoint('/api/v1/dataset?test1=test&test2=test', { method: 'put' });
@@ -73,7 +73,7 @@ describe('Endpoint dispatch tests', () => {
         result.status.should.equal(200);
     });
 
-    it('Send queries, queries should move on new GET request', async () => {
+    it('External request\'s query params are passed along to the internal call on GET requests', async () => {
         await updateVersion();
         await createEndpoint(changeMethod('GET'));
         createMockEndpoint('/api/v1/dataset?test1=test&test2=test&loggedUser=null', { method: 'get' });
@@ -82,7 +82,7 @@ describe('Endpoint dispatch tests', () => {
         result.status.should.equal(200);
     });
 
-    it('Send queries, queries should move on new PATCH request', async () => {
+    it('External request\'s query params are passed along to the internal call on PATCH requests', async () => {
         await updateVersion();
         await createEndpoint(changeMethod('PATCH'));
         createMockEndpoint('/api/v1/dataset?test1=test&test2=test', { method: 'patch' });
@@ -91,7 +91,7 @@ describe('Endpoint dispatch tests', () => {
         result.status.should.equal(200);
     });
 
-    it('Send queries, queries should move on new DELETE request', async () => {
+    it('External request\'s query params are passed along to the internal call on DELETE requests', async () => {
         await updateVersion();
         await createEndpoint(changeMethod('DELETE'));
         createMockEndpoint('/api/v1/dataset?test1=test&test2=test&loggedUser=null', { method: 'delete' });
@@ -100,7 +100,7 @@ describe('Endpoint dispatch tests', () => {
         result.status.should.equal(200);
     });
 
-    it('Send body, body should ignored when request is neither PUT nor POST nor PATCH', async () => {
+    it('External request\'s body content should be absent the internal call on GET requests', async () => {
         await updateVersion();
         await createEndpoint(changeMethod('GET'));
         createMockEndpoint('/api/v1/dataset?loggedUser=null', { method: 'get' });
@@ -109,7 +109,16 @@ describe('Endpoint dispatch tests', () => {
         result.text.should.equal('ok');
     });
 
-    it('Send body, body should be present in POST redirect request', async () => {
+    it('External request\'s body content should be absent the internal call on DELETE requests', async () => {
+        await updateVersion();
+        await createEndpoint(changeMethod('DELETE'));
+        createMockEndpoint('/api/v1/dataset?loggedUser=null', { method: 'delete' });
+        const result = await microservice.delete('/api/v1/dataset').send({ test1: 'test2' });
+        result.status.should.equal(200);
+        result.text.should.equal('ok');
+    });
+
+    it('External request\'s body content should be present the internal call on POST requests', async () => {
         await updateVersion();
         await createEndpoint();
         createMockEndpointWithBody('/api/v1/dataset', { body: { test1: 'test2', loggedUser: null } });
@@ -118,7 +127,7 @@ describe('Endpoint dispatch tests', () => {
         result.text.should.equal('ok');
     });
 
-    it('Send body, body should be present in PATCH redirect request', async () => {
+    it('External request\'s body content should be present the internal call on PATCH requests', async () => {
         await updateVersion();
         await createEndpoint(changeMethod('PATCH'));
         createMockEndpointWithBody('/api/v1/dataset', { method: 'patch', body: { test1: 'test2', loggedUser: null } });
@@ -127,7 +136,7 @@ describe('Endpoint dispatch tests', () => {
         result.text.should.equal('ok');
     });
 
-    it('Send body, body should be present in PUT redirect request', async () => {
+    it('External request\'s body content should be present the internal call on PUT requests', async () => {
         await updateVersion();
         await createEndpoint(changeMethod('PUT'));
         createMockEndpointWithBody('/api/v1/dataset', { method: 'put', body: { test1: 'test2', loggedUser: null } });
@@ -136,7 +145,7 @@ describe('Endpoint dispatch tests', () => {
         result.text.should.equal('ok');
     });
 
-    it('Send post request should be redirected to get request', async () => {
+    it('Forwarding an external POST request to an internal GET request should work as intended (happy case)', async () => {
         await updateVersion();
         await createEndpoint({ ...endpointTest, redirect: [{ ...endpointTest.redirect[0], method: 'get' }] });
         createMockEndpoint('/api/v1/dataset', { method: 'get' });
@@ -145,7 +154,7 @@ describe('Endpoint dispatch tests', () => {
         result.text.should.equal('ok');
     });
 
-    it('Send custom headers which is allowed, headers should be present in redirected request', async () => {
+    it('External request\'s headers should be present the internal request', async () => {
         await updateVersion();
         await createEndpoint();
         createMockEndpointWithHeaders('/api/v1/dataset', { headers: { location: 'test2' } });
@@ -154,7 +163,7 @@ describe('Endpoint dispatch tests', () => {
         result.text.should.equal('ok');
     });
 
-    it('Send stream request', async () => {
+    it('External stream requests are supported', async () => {
         await updateVersion();
         await createEndpoint({ binary: true });
         createMockEndpoint('/api/v1/dataset');
@@ -168,7 +177,7 @@ describe('Endpoint dispatch tests', () => {
         result.status.should.equal(404);
     });
 
-    it('Create endpoint with pathRegex should success', async () => {
+    it('Endpoint with pathRegex are matched to external requests and return a 200 HTTP code (positive test)', async () => {
         await updateVersion();
         // eslint-disable-next-line no-useless-escape
         await createEndpoint({ pathRegex: new RegExp('^/api/v1/dataset/[0-9]*$') });
@@ -178,7 +187,7 @@ describe('Endpoint dispatch tests', () => {
         result.text.should.equal('ok');
     });
 
-    it('check path regex is working right', async () => {
+    it('Endpoint with pathRegex are matched to external requests and return a 200 HTTP code (negative test)', async () => {
         await updateVersion();
         // eslint-disable-next-line no-useless-escape
         await createEndpoint({ pathRegex: new RegExp('^/api/v1/dataset/[0-9]*$') });
