b249cb8d799d1bdfdd44a6a28ed698f9d4cddf32
Tiago Garcia <tiagojsag@gmail.com>
2019-09-20 15:04:38 +0200

    Restructure tests to return promise instead of calling done()

--- app/test/e2e/ct-oauth/ct-oauth-recover-password-request-html.spec.js
+++ app/test/e2e/ct-oauth/ct-oauth-recover-password-request-html.spec.js
@@ -119,62 +119,6 @@ describe('OAuth endpoints tests - Recover password request - HTML version', () =
         response.text.should.include(`Email sent`);
     });
 
-    it('Recover password request with correct email should return OK - HTML format', async () => {
-        nock('https://api.sparkpost.com')
-            .post('/api/v1/transmissions', (body) => {
-                const expectedRequestBody = {
-                    content: {
-                        template_id: 'recover-password'
-                    },
-                    recipients: [
-                        {
-                            address: {
-                                email: 'potato@gmail.com'
-                            }
-                        }
-                    ],
-                    substitution_data: {
-                        fromName: 'GFW',
-                        appName: 'GFW',
-                        logo: 'https://www.globalforestwatch.org/packs/gfw-9c5fe396ee5b15cb5f5b639a7ef771bd.png'
-                    }
-                };
-
-                body.should.have.property('substitution_data').and.be.an('object');
-                body.substitution_data.should.have.property('urlRecover').and.include(`${process.env.PUBLIC_URL}/auth/reset-password/`);
-                body.substitution_data.should.have.property('urlRecover').and.include('origin=gfw');
-
-                delete body.substitution_data.urlRecover;
-
-                body.should.deep.equal(expectedRequestBody);
-
-                return isEqual(body, expectedRequestBody);
-            })
-            .once()
-            .reply(200, {
-                results: {
-                    total_rejected_recipients: 0,
-                    total_accepted_recipients: 1,
-                    id: 11668787484950529
-                }
-            });
-
-        await new UserModel({
-            email: 'potato@gmail.com'
-        }).save();
-
-        const response = await requester
-            .post(`/auth/reset-password?origin=gfw`)
-            .type('form')
-            .send({
-                email: 'potato@gmail.com'
-            });
-
-        response.status.should.equal(200);
-        response.header['content-type'].should.equal('text/html; charset=utf-8');
-        response.text.should.include(`Email sent`);
-    });
-
     after(async () => {
         closeTestAgent();
     });
