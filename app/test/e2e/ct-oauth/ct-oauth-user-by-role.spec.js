3b0e0ee5b2b972defd84d179bbd3f03d823fbfcc
Tiago Garcia <tiagojsag@gmail.com>
2019-10-15 12:04:13 +0200

    Add tests to GET users ids by role endpoint

---
+++ app/test/e2e/ct-oauth/ct-oauth-user-by-role.spec.js
@@ -0,0 +1,129 @@
+/* eslint-disable no-unused-expressions */
+const nock = require('nock');
+const chai = require('chai');
+
+const UserModel = require('plugins/sd-ct-oauth-plugin/models/user.model');
+
+const { createUser } = require('./../utils');
+const { getTestAgent, closeTestAgent } = require('./../test-server');
+const { TOKENS } = require('./../test.constants');
+
+const should = chai.should();
+
+let requester;
+
+nock.disableNetConnect();
+nock.enableNetConnect(process.env.HOST_IP);
+
+describe('GET users ids by role', () => {
+
+    before(async () => {
+        if (process.env.NODE_ENV !== 'test') {
+            throw Error(`Running the test suite with NODE_ENV ${process.env.NODE_ENV} may result in permanent data loss. Please use NODE_ENV=test.`);
+        }
+
+        requester = await getTestAgent();
+
+        UserModel.deleteMany({}).exec();
+
+        nock.cleanAll();
+    });
+
+    it('Get users ids by role without being logged in returns a 401', async () => {
+        const response = await requester
+            .get(`/auth/user/ids/USER`);
+
+        response.status.should.equal(401);
+    });
+
+    it('Get users ids by role while being logged in as a USER returns a 400 error', async () => {
+        const response = await requester
+            .get(`/auth/user/ids/USER`)
+            .set('Authorization', `Bearer ${TOKENS.USER}`);
+
+        response.status.should.equal(403);
+        response.body.should.have.property('errors').and.be.an('array');
+        response.body.errors[0].should.have.property('detail').and.equal(`Not authorized`);
+    });
+
+    it('Get users ids by role while being logged in as a MANAGER returns a 400 error', async () => {
+        const response = await requester
+            .get(`/auth/user/ids/USER`)
+            .set('Authorization', `Bearer ${TOKENS.MANAGER}`);
+
+        response.status.should.equal(403);
+        response.body.should.have.property('errors').and.be.an('array');
+        response.body.errors[0].should.have.property('detail').and.equal(`Not authorized`);
+    });
+
+    it('Get users ids by role while being logged in as an ADMIN returns a 400 error', async () => {
+        const response = await requester
+            .get(`/auth/user/ids/USER`)
+            .set('Authorization', `Bearer ${TOKENS.ADMIN}`);
+
+        response.status.should.equal(403);
+        response.body.should.have.property('errors').and.be.an('array');
+        response.body.errors[0].should.have.property('detail').and.equal(`Not authorized`);
+    });
+
+    it('Get users ids by role with an invalid role returns a 422', async () => {
+        const response = await requester
+            .get(`/auth/user/ids/FOO`)
+            .set('Authorization', `Bearer ${TOKENS.MICROSERVICE}`);
+
+        response.status.should.equal(422);
+        response.body.should.have.property('errors').and.be.an('array');
+        response.body.errors[0].should.have.property('detail').and.equal(`Invalid role FOO provided`);
+    });
+
+    it('Get users ids by role with a valid role and no users on the database returns a 200 response and an empty array', async () => {
+        const response = await requester
+            .get(`/auth/user/ids/USER`)
+            .set('Authorization', `Bearer ${TOKENS.MICROSERVICE}`);
+
+        response.status.should.equal(200);
+
+        response.body.should.have.property('data').and.eql([]);
+    });
+
+    it('Get users ids by role with a valid role returns a 200 response with the users ids (happy case, single user)', async () => {
+        const userOne = await new UserModel(createUser()).save();
+
+        const response = await requester
+            .get(`/auth/user/ids/USER`)
+            .set('Authorization', `Bearer ${TOKENS.MICROSERVICE}`);
+
+        response.status.should.equal(200);
+        response.body.should.have.property('data').and.be.an('array').and.length(1);
+
+        response.body.should.have.property('data').and.eql([userOne.id]);
+    });
+
+    it('Get users ids by role with a valid role returns a 200 response with the users ids (happy case, multiple users)', async () => {
+        await new UserModel(createUser(['rw'], 'ADMIN')).save();
+        await new UserModel(createUser(['rw'], 'MANAGER')).save();
+        const userThree = await new UserModel(createUser(['rw'], 'USER')).save();
+        const userFour = await new UserModel(createUser(['rw'], 'USER')).save();
+
+        const response = await requester
+            .get(`/auth/user/ids/USER`)
+            .set('Authorization', `Bearer ${TOKENS.MICROSERVICE}`);
+
+        response.status.should.equal(200);
+        response.body.should.have.property('data').and.be.an('array').and.length(2);
+
+        response.body.should.have.property('data').and.eql([userThree.id, userFour.id]);
+    });
+
+    after(() => {
+        closeTestAgent();
+    });
+
+    afterEach(async () => {
+        await UserModel.deleteMany({}).exec();
+
+        if (!nock.isDone()) {
+            throw new Error(`Not all nock interceptors were used: ${nock.pendingMocks()}`);
+        }
+    });
+});
