c1d053dbb8a4626a374a2217715b61a2f6b0f894
Ed Brett <e.j.a.brett@gmail.com>
2020-01-26 19:37:04 +0100

    fix grammar error with email exists error

--- app/test/e2e/ct-oauth/ct-oauth-sign-up-with-json-content-type.spec.js
+++ app/test/e2e/ct-oauth/ct-oauth-sign-up-with-json-content-type.spec.js
@@ -180,7 +180,7 @@ describe('OAuth endpoints tests - Sign up with JSON content type', () => {
         response.should.be.json;
         response.body.should.have.property('errors').and.be.an('array');
         response.body.errors[0].status.should.equal(422);
-        response.body.errors[0].detail.should.equal('Email exist');
+        response.body.errors[0].detail.should.equal('Email exists');
     });
 
     it('Confirming a user\'s account using the email token should be successful (user without app)', async () => {
@@ -222,7 +222,7 @@ describe('OAuth endpoints tests - Sign up with JSON content type', () => {
         response.should.be.json;
         response.body.should.have.property('errors').and.be.an('array');
         response.body.errors[0].status.should.equal(422);
-        response.body.errors[0].detail.should.equal('Email exist');
+        response.body.errors[0].detail.should.equal('Email exists');
     });
 
 
