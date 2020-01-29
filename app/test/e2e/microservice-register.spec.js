49b505be9380e4281685564ab8f8a198d71497b3
stasgalin <stas.galin@do-it.co>
2020-01-29 12:41:46 +0200

    fixing microservice tests to new behavior of auth

--- app/test/e2e/microservice-register.spec.js
+++ app/test/e2e/microservice-register.spec.js
@@ -6,7 +6,7 @@ const Microservice = require('models/microservice.model');
 const Endpoint = require('models/endpoint.model');
 
 const { getTestAgent, closeTestAgent } = require('./test-server');
-const { TOKENS } = require('./test.constants');
+const { createUserAndToken } = require('./utils/helpers');
 
 let requester;
 
@@ -118,24 +118,28 @@ describe('Microservices endpoints', () => {
     });
 
     it('Authorized status check and registered microservice (happy case)', async () => {
+        const { token } = await createUserAndToken({ role: 'ADMIN' });
+
         const response = await requester.get(`/api/v1/microservice`)
             .send()
-            .set('Authorization', `Bearer ${TOKENS.ADMIN}`);
+            .set('Authorization', `Bearer ${token}`);
 
         response.status.should.equal(200);
     });
 
     it('Deleting a microservice should delete endpoints and delete microservice document in the database (happy case)', async () => {
+        const { token } = await createUserAndToken({ role: 'ADMIN' });
+
         (await Microservice.find()).should.have.lengthOf(1);
         (await Endpoint.find({ toDelete: true })).should.have.lengthOf(0);
 
         const existingMicroservice = await requester.get(`/api/v1/microservice`)
-            .set('Authorization', `Bearer ${TOKENS.ADMIN}`)
+            .set('Authorization', `Bearer ${token}`)
             .send();
 
         const response = await requester.delete(`/api/v1/microservice/${existingMicroservice.body[0]._id}`)
             .send()
-            .set('Authorization', `Bearer ${TOKENS.ADMIN}`);
+            .set('Authorization', `Bearer ${token}`);
 
         response.status.should.equal(200);
 
@@ -144,9 +148,11 @@ describe('Microservices endpoints', () => {
     });
 
     it('Getting endpoints for registered microservices should return a list of available endpoints (happy case)', async () => {
+        const { token } = await createUserAndToken({ role: 'ADMIN' });
+
         const response = await requester.get(`/api/v1/endpoint`)
             .send()
-            .set('Authorization', `Bearer ${TOKENS.ADMIN}`);
+            .set('Authorization', `Bearer ${token}`);
 
         response.status.should.equal(200);
         response.body.should.be.an('array').and.have.lengthOf(3);
